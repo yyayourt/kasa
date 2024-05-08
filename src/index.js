7454fb7e4b289405640c913ac61dbf97b342c801
yyayourt <yanisnasri30@gmail.com>
2024-05-08 15:14:16 +0200

    scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./assets/SCSS/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Header from "./components/Header";
